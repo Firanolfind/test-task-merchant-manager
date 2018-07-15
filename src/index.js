7481ae01240bfce4d41c336136ffad2299ac59f3
Firanolfind <firanolfind@gmail.com>
2018-07-16 00:50:02 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+import App from './App';
+import configureStore from './configureStore';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+  	<BrowserRouter>
+      <App/>
+  	</BrowserRouter>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
