b8246886a7b8765bfc29e51ec72f4a812bf7d215
Firanolfind <firanolfind@gmail.com>
2018-07-18 03:13:33 +0300

    connect Merchant List to redux

--- src/scenes/Merchant/List.js
+++ src/scenes/Merchant/List.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect }            from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Button } from 'reactstrap';
+
+import * as actions from '~/redux/merchants/actions';
 
 class MerchantList extends Component {
   render () {
@@ -14,4 +18,11 @@ class MerchantList extends Component {
   }
 }
 
-export default MerchantList;
+export default connect(
+    state => ({
+        items: state.merchants
+    }),
+    dispatch => ({
+        actions: bindActionCreators(actions, dispatch)
+    })
+)(MerchantList);
