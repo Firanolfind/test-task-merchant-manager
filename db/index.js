33cf0e0f28040ff082138bce32bccbb44bab6aab
Firanolfind <firanolfind@gmail.com>
2018-07-19 17:29:15 +0300

    upgrade seed for merchants and bids added

--- db/index.js
+++ db/index.js
@@ -1,19 +1,39 @@
 const faker = require('faker');
 
+const cars = ["Chevrolet","Cadillac","Ford","Chrysler","Dodge","Jeep","Tesla","Toyota","Honda","Nissan","Audi","Mercedes Benz","BMW","Volkswagen","Porsche","Jaguar","Aston Martin","Land Rover","Bentley","Mini","Rolls Royce","Fiat","Lamborghini","Maserati","Ferrari","Bugatti","Kia","Hyandai"];
+const randRange = (max, min=0) => ~~(Math.random() * (max - min + 1)) + min;
+
 module.exports = function() {
 
+  const merchantsCount = 100;
   const db = {};
+  var bids = [];
+  var bidsCursor = 0;
 
-  db.merchants = [...Array(2)].map((item, id)=> ({
-    id: id + 1,
-    firstname:  faker.name.firstName(),
-    lastname:   faker.name.lastName(),
-    avatarUrl:  faker.internet.avatar(),
-    email:      faker.internet.email(),
-    phone:      faker.phone.phoneNumber(),
-    hasPremium: Math.random() >= 0.5,
-    bids:       []
+  const genBids = amount => [...Array(amount)].map((item, id)=> ({
+    id:         ++bidsCursor,
+    carTitle:   cars[randRange(cars.length)],
+    amount:     faker.random.number(),
+    created:    faker.date.past()
   }));
+
+  db.merchants = [...Array(merchantsCount)].map((item, id)=> {
+    const newBids = genBids(randRange(2, 10));
+    bids = [...newBids];
+
+    return {
+      id: id + 1,
+      firstname:  faker.name.firstName(),
+      lastname:   faker.name.lastName(),
+      avatarUrl:  faker.internet.avatar(),
+      email:      faker.internet.email(),
+      phone:      faker.phone.phoneNumber(),
+      hasPremium: Math.random() >= 0.5,
+      bids:       newBids
+    }
+  });
+
+  db.bids = bids;
 
   return db;
 }
